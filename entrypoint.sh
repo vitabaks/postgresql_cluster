4827f7a748f712cda10ebc36663ca0768b1954bc
Vitaliy Kukharik <vitabaks@gmail.com>
2024-07-09 14:18:43 +0300

    entrypoint.sh: Add support for base64 encoded input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+is_base64() {
+  # Check if input is base64 encoded
+  if [[ "$1" =~ ^[A-Za-z0-9+/=]+$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 # Check if ANSIBLE_INVENTORY_JSON is set and create inventory.json if it is
 if [[ -n "${ANSIBLE_INVENTORY_JSON}" ]]; then
   echo "Creating inventory.json with the content of ANSIBLE_INVENTORY_JSON"
-  echo "${ANSIBLE_INVENTORY_JSON}" > /postgresql_cluster/inventory.json
+  if is_base64 "${ANSIBLE_INVENTORY_JSON}"; then
+    echo "Decoding base64 ANSIBLE_INVENTORY_JSON"
+    echo "${ANSIBLE_INVENTORY_JSON}" | base64 -d > /postgresql_cluster/inventory.json
+  else
+    echo "${ANSIBLE_INVENTORY_JSON}" > /postgresql_cluster/inventory.json
+  fi
   # Set ANSIBLE_INVENTORY environment variable
   export ANSIBLE_INVENTORY=/postgresql_cluster/inventory.json
   # Set ANSIBLE_SSH_ARGS environment variable
@@ -14,7 +28,12 @@ fi
 if [[ -n "${SSH_PRIVATE_KEY_CONTENT}" ]]; then
   echo "Creating SSH private key file with the content of SSH_PRIVATE_KEY_CONTENT"
   mkdir -p /root/.ssh
-  echo "${SSH_PRIVATE_KEY_CONTENT}" > /root/.ssh/id_rsa
+  if is_base64 "${SSH_PRIVATE_KEY_CONTENT}"; then
+    echo "Decoding base64 SSH_PRIVATE_KEY_CONTENT"
+    echo "${SSH_PRIVATE_KEY_CONTENT}" | base64 -d > /root/.ssh/id_rsa
+  else
+    echo "${SSH_PRIVATE_KEY_CONTENT}" > /root/.ssh/id_rsa
+  fi
   chmod 600 /root/.ssh/id_rsa
   # Set ANSIBLE_PRIVATE_KEY_FILE environment variable
   export ANSIBLE_PRIVATE_KEY_FILE=/root/.ssh/id_rsa
