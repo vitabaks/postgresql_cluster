604b49c9cd10a340212e6da36522a43b8fec6687
Vitaliy Kukharik <37010174+vitabaks@users.noreply.github.com>
2024-12-11 15:12:27 +0500

    Update console/db/migrations/20241205103951_2.1.0.sql

--- console/db/migrations/20241205103951_2.1.0.sql
+++ console/db/migrations/20241205103951_2.1.0.sql
@@ -23,7 +23,7 @@ WHERE extension_name IN (
 -- Adds shared_cpu BOOLEAN field to cloud_instances
 -- ref: https://github.com/vitabaks/autobase/issues/784
 ALTER TABLE ONLY public.cloud_instances
-ADD COLUMN 'shared_cpu' BOOLEAN DEFAULT FALSE;
+ADD COLUMN shared_cpu BOOLEAN DEFAULT FALSE;
 
 -- Update AWS shared vCPU instances
 UPDATE public.cloud_instances
